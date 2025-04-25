b1e9af20f01c1b635c5efc09f9e90ecaa4739d9d
MineMineMine221 <114082225+MineMineMine221@users.noreply.github.com>
2025-04-25 16:48:26 +0700

    up

--- Assets/Scripts/TeamManager.cs
+++ Assets/Scripts/TeamManager.cs
@@ -41,12 +41,10 @@ public class TeamManager : MonoBehaviourPunCallbacks
             if (team.PlayerCount(playerData.teamName) < maxTeamCount && team.TryToAddPlayer(playerData))
             {
                 report.text = "Add Team Complete";
-
             }
             else
             {
                 report.text = "Add Team Fail";
-
             }
         }
         else
@@ -70,8 +68,6 @@ public class TeamManager : MonoBehaviourPunCallbacks
 
     }
 
-
-
     private void UpdateTeamToOther()
     {
         if (!PhotonNetwork.IsMasterClient) return;
@@ -84,8 +80,6 @@ public class TeamManager : MonoBehaviourPunCallbacks
 
         PhotonNetwork.CurrentRoom.SetCustomProperties(playerdata);
     }
-
-
 
     public override void OnRoomPropertiesUpdate(Hashtable propertiesThatChanged)
     {
@@ -105,9 +99,6 @@ public class TeamManager : MonoBehaviourPunCallbacks
             }
         }
     }
-
-
-
 
 }
 
